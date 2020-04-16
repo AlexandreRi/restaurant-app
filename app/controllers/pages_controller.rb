c7facda942183c02da26f8859d0b34442421bf6e
Alexandre Rinaldi <alexandre.rinaldi@essec.edu>
2020-04-16 12:10:51 +0200

    Add address to Restaurant

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,17 @@
 class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def contact
+    @members = ['Thanh', 'Dimitri', 'Germain', 'Damien', 'Julien']
+    if params[:member]
+      @members = @members.select { |member| member.starts_with?(params[:member]) }
+    end
+    if params[:member] && (params[:member].to_s != params[:member].to_s.capitalize)
+      redirect_to "http://localhost:3000/contact?member=#{params[:member].to_s.capitalize}"
+    end
+  end
 end
